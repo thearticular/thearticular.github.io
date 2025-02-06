68170c8613d3eabd29bc9617b2067987be89c431
thearticular <theromitsingh@gmail.com>
2025-02-06 20:07:47 +0530

    up

--- thearticular/wp-content/layers/worker.js
+++ thearticular/wp-content/layers/worker.js
@@ -3,15 +3,11 @@ self.onmessage = function (e)
   const { data, targetSamples } = e.data;
   const sampleSize = Math.floor(data.length / targetSamples);
   const downsampledData = new Float32Array(targetSamples);
+
   for (let i = 0; i < targetSamples; i++)
   {
-    let blockStart = i * sampleSize;
-    let sum = 0;
-    for (let j = 0; j < sampleSize; j++)
-    {
-      sum += Math.abs(data[blockStart + j]);
-    }
-    downsampledData[i] = sum / sampleSize;
+    downsampledData[i] = Math.abs(data[i * sampleSize]);
   }
+
   self.postMessage({ downsampledData });
 };
